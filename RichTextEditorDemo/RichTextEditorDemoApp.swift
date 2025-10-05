d8c4d65154f0885d8b431fd3110e53beba7bc4a8
Divyesh Vekariya <divyeshkvekariya@gmail.com>
2025-10-05 12:32:55 +0530

    Initial commit

---
+++ RichTextEditorDemo/RichTextEditorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichTextEditorDemoApp.swift
+//  RichTextEditorDemo
+//
+//  Created by DK on 05/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RichTextEditorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
